6d5c6189c61a0bf5e80d13480bac6166ed24480d
Chrovo <chrovo1@gmail.com>
2021-07-17 17:08:00 -0400

    Updated pic_to_url method and typehinted stuff

--- tracemoe/sync_tracemoe/tracemoe_s.py
+++ tracemoe/sync_tracemoe/tracemoe_s.py
@@ -8,7 +8,7 @@ from typing import Optional, Union
 import requests
 
 from ..sync_tracemoe_errors import (
-    ConcurrencyOrRatelimit,
+    InvalidURL,
     TraceMoeError,
 )
 from ..tracemoe_response import TraceMoeResponse
@@ -18,5 +18,25 @@ class TraceMoe:
     def __init__(self, *, token:Optional[str]) -> None:
         self.token = token
 
-    def pic_to_url(self, url:str) -> TraceMoeResponse:
-        pass #Do later
+    def pic_to_url(
+        self, url:str, *, 
+        anilist_id:Optional[int]=None, 
+        cut_borders:Optional[bool]=False,
+    ) -> TraceMoeResponse: # Add api token stuff later
+        base_url = "https://api.trace.moe/search?"
+
+        if anilist_id is not None:
+            base_url+=f"anilistID={anilist_id}"
+
+        if cut_borders is not None:
+            check = f"&cutBorders&" if anilist_id is not None else f"cutBorders&"
+            base_url+=check
+        
+        base_url+=f"url={url}"
+        response = requests.get(base_url)
+        if response.ok:
+            return TraceMoeResponse(response.json())
+        elif response.status_code == 400:
+            raise InvalidURL(f'Invalid URL "{url}" provided.')
+        else:
+            pass # Add more exceptions later.
--- tracemoe/sync_tracemoe_errors.py
+++ tracemoe/sync_tracemoe_errors.py
@@ -1,5 +1,5 @@
 class TraceMoeError(Exception):
-    def __init__(self, message):
+    def __init__(self, message:str) -> None:
         self.message = message
 
 class InvalidURL(TraceMoeError): # If a 400 status code occurs when using a url to find an image.
