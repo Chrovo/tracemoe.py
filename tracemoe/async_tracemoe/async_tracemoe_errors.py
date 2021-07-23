5bd64680954df6df1cebf96ace018ee23e4a425b
Chrovo <chrovo1@gmail.com>
2021-07-23 15:47:55 -0400

    Update async tracemoe

---
+++ tracemoe/async_tracemoe/async_tracemoe_errors.py
@@ -0,0 +1,15 @@
+class TraceMoeError(Exception):
+    def __init__(self, message:str) -> None:
+        self.message = message
+
+class InvalidURL(TraceMoeError):
+    pass
+
+class Ratelimited(TraceMoeError):
+    pass
+
+class ConcurrencyOrRatelimit(TraceMoeError):
+    pass
+
+class InvalidAPIKey(TraceMoeError):
+    pass
---
+++ tracemoe/async_tracemoe/tracemoe_a.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+import asyncio
+import aiohttp
+from typing import Optional, Union
+
+from async_tracemoe_response import AsyncTraceMoeResponse
+from async_tracemoe_errors import (
+    InvalidURL, 
+    Ratelimited, 
+    ConcurrencyOrRatelimit, 
+    InvalidAPIKey,
+)
+
+class AsyncTraceMoe:
+
+    def __init__(self, *, token:Optional[str]=None) -> None:
+        self.token = token # Add more stuff later
+        self._error_dict = {
+            429:Ratelimited("To many HTTP requests, you are blocked from accessing the trace.moe api for 60 minutes."),
+            402:ConcurrencyOrRatelimit("Max concurrency of use reached or you have been ratelimited."),
+            403:InvalidAPIKey("The API key provided was invalid."),
+            400:InvalidURL(f'Invalid image url'),
+        }
+    
+    async def url_search(
+        self, 
+        url:str, *, 
+        anilist_id:Optional[int]=None, 
+        cut_borders:Optional[bool]=False,
+    ) -> AsyncTraceMoeResponse:
+        base_url = "https://api.trace.moe/search?"
+
+        if anilist_id is not None:
+            base_url+=f"anilistID={anilist_id}"
+
+        if cut_borders:
+            check = f"&cutBorders&" if anilist_id is not None else f"cutBorders&"
+            base_url+=check
+        
+        base_url+=f"url={url}"
+        
+        headers:Union[dict[str, str], dict[str, None]] = {"x-trace-key":self.token} if self.token is not None else {"x-trace-key":None}
+        async with aiohttp.ClientSession as ses: # Make this part better later.
+            async with ses.get(f'{base_url}', headers=headers) as r:
+                if r.status in range(200,300):
+                    data = await r.json()
+                    return AsyncTraceMoeResponse(data)
+                else:
+                    raise self._error_dict.get(r.status, r.raise_for_status()) # May change raise_for_status()
